9802568995321ff8a5b5d45e7e47f763754fde5c
jodo993 <josephdo993@yahoo.com>
2017-10-08 02:27:38 -0700

    Chapter 1 - Sales Tax

---
+++ Proj1_Prob1.py
@@ -0,0 +1,27 @@
+# Joseph Do
+# Project 1 - 6. Sales Tax
+# This program will compute the sales tax and total sale
+
+# Gather the amount of the purchase
+amountOfPurchase = input("Please enter the amount of purchase: ")
+amountOfPurchase = float(amountOfPurchase)
+
+# Tax Rate
+STATE_SALES_TAX = 0.05
+COUNTY_SALES_TAX = 0.025
+
+# Calculate each sales tax, total sales tax, and total overall
+itemStateSalesTax = amountOfPurchase * STATE_SALES_TAX
+itemCountySalesTax = amountOfPurchase * COUNTY_SALES_TAX
+totalSalesTax = itemStateSalesTax + itemCountySalesTax
+totalSales = totalSalesTax + amountOfPurchase
+
+# Display Results
+print("Amount of Purchase: ","$%.2f" % amountOfPurchase)
+print("State Sales Tax:    ","$%.2f" % itemStateSalesTax)
+print("County Sales Tax:   ","$%.2f" % itemCountySalesTax)
+print("Total Sales Tax:    ","$%.2f" % totalSalesTax)
+print("Total Sale:         ","$%.2f" % totalSales)
+
+
+
